b95a20921560d620db44ff662e46969b279e3618
Alexander Girin <alexander.girin@informatik.hs-fulda.de>
2021-01-13 16:05:24 +0100

    Add generate random word

--- freiloos/src/game/Hangman.java
+++ freiloos/src/game/Hangman.java
@@ -1,5 +1,5 @@
 package game;
-import java.util.Scanner;
+import java.util.*;
 
 // Hangman Projekt von Dawid Voronov und Alexander Girin || AI1001
 
@@ -14,14 +14,17 @@ public class Hangman {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     String eingabe = in.next();	
-    int x = 1; // aendern der variable in random
-    for (int i = 0; i < WOERTER[x].length(); ) { 
+   
+    Random rnd = new Random(); 
+    int zufaelligesWort = rnd.nextInt(WOERTER.length);
+    
+    for (int i = 0; i < WOERTER[zufaelligesWort].length(); ) { 
       while (true) {
         if (!istGueltig(eingabe)){
           System.out.println("Ungueltige Eingabe. Bitte nur einen Buchstaben eingeben!sdf");
           eingabe = in.next();
         } else {
-    	    System.out.println("true");
+    	    System.out.println(WOERTER[zufaelligesWort]);
     	    i++;
     	    break;
         } 
